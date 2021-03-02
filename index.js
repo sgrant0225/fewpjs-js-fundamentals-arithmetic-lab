3890af91253d676b74759d8843f99369553418bc
github username <sharnellgrant@gmail.com>
2021-03-02 15:28:15 -0500

    commit tree 159aaa131d7b1ec468ec2db6b05eafaa816f4c65

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
-// Write your code here
+
+// is an equation whose multiplied results will equal 62:
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+
+// generates a random integer greater than 0:    
+const random = Math.floor(Math.random() * 10) + 1;
+
+// is an equation that calculates a remainder that is equal to 4:
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4;
+
+// will return 20 as the highest number in the set:
+const max = Math.max(5, 10, 15, 20);
